801409f50992896361def85196c722c1c703f331
Ryoon2K <reinisdoesminecraft@gmail.com>
2025-01-06 17:43:24 +0200

    Made settings items alternate colors and removed the underlining color.

--- addons/autofoldercolor/essential/dock/components/afc_settings/afc_settings_item.gd
+++ addons/autofoldercolor/essential/dock/components/afc_settings/afc_settings_item.gd
@@ -4,18 +4,21 @@ class_name AFCSettingsItem extends VBoxContainer
 @onready var label: Label = %Label
 @onready var check_box: CheckButton = %CheckBox
 
-
 var property_name:String
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	_init_color()
 	_init_text()
 	if AFCDock.settings.get(property_name): check_box.set_pressed_no_signal(true)
 	else: check_box.set_pressed_no_signal(false)
 	
 	check_box.connect("toggled",_change_setting)
-	
-	$HSeparator.set("theme_override_styles/separator",$HSeparator.get("theme_override_styles/separator").duplicate())
+
+func _init_color()->void:
+	if get_parent().get_children().find(self)%2:
+		%ColorRect.color = Color("#363636")
+	else: %ColorRect.color = Color("#242424")
 
 func _init_text()->void:
 	var _text := property_name
